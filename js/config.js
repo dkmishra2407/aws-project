7f0c0012142f59d5948494ff2f052c5259a613a8
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2025-06-18 21:10:56 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_Srs1rIUw6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2srteq2jve70f3klbf7rdrkbrk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-southeast-2' // e.g. us-east-2
+        userPoolId: 'ap-south-1_WTXWTTnQJ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '40s94pn116l1q9d8fm8iu86ttj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://gex92860yl.execute-api.ap-southeast-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+        invokeUrl: 'https://f4jh9foffi.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
 };
